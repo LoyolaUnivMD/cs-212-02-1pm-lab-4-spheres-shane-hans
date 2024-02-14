419a121f2376d2972354aaadb3e236cc01a32c8d
Hans <htvanlierop@loyola.edu>
2024-02-14 13:47:21 -0500

    Created other methods for Spheres

--- Main.java
+++ Main.java
@@ -13,8 +13,27 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-       System.out.println("Music of the Spheres");
+       System.out.println("Music of the Spheres\n");
 
+       // Initialize three spheres
+       Sphere sun = new Sphere("sun", 43.26, 0.0, 0.0, 0.0);
+       Sphere earth = new Sphere("earth", 0.395, 26.0, 42.0, 79.0);
+       Sphere planetX = new Sphere("planetX", 1.4, 25.3, 43.1, 77.8);
 
+       // Print surface area and volume of the three spheres
+        System.out.println(sun.name + ":\nSurface Area - " + sun.surfaceArea() + "\nVolume - " + sun.volume() + "\n");
+        System.out.println(earth.name + ":\nSurface Area - " + earth.surfaceArea() + "\nVolume - " + earth.volume() + "\n");
+        System.out.println(planetX.name + ":\nSurface Area - " + planetX.surfaceArea() + "\nVolume - " + planetX.volume() + "\n");
+
+        // Collision check
+        if (sun.collision(earth.radius, earth.x, earth.y, earth.z)) {
+            System.out.println("The sun and earth's spheres have collided.");
+        }
+        if (sun.collision(planetX.radius, planetX.x, planetX.y, planetX.z)) {
+            System.out.println("The sun and planetX's spheres have collided.");
+        }
+        if (planetX.collision(earth.radius, earth.x, earth.y, earth.z)) {
+            System.out.println("PlanetX and earth's spheres have collided.");
+        }
     }
 }
--- Sphere.java
+++ Sphere.java
@@ -12,9 +12,11 @@
 
 
 class Sphere {
-    private double radius, x, y, z;
+    public String name;
+    public double radius, x, y, z;
     // General Constructor
-    public Sphere(double radius, double x, double y, double z) {
+    public Sphere(String name, double radius, double x, double y, double z) {
+        this.name = name;
         this.radius = radius;
         this.x = x;
         this.y = y;
@@ -23,6 +25,7 @@ class Sphere {
 
     // Default Constructor
     public Sphere(){
+        this.name = "Unknown";
         this.radius = 1;
         this.x = 0;
         this.y = 0;
@@ -34,8 +37,17 @@ class Sphere {
         return 4 * Math.PI * Math.pow(radius, 2);
     }
     // Volume method
+    public double volume() {
+        return (double) 4/3 * Math.PI * Math.pow(radius, 3);
+    }
     // Distance method
+    public double distance(double x_2, double y_2, double z_2) {
+        return Math.sqrt(Math.pow((x - x_2), 2) + Math.pow((y - y_2), 2) + Math.pow((z - z_2), 2));
+    }
     // Collision method
+    public boolean collision(double radius_2, double x_2, double y_2, double z_2){
+        return distance(x_2, y_2, z_2) < (radius + radius_2);
+    }
     public static void main(String[] args) {
        System.out.println("Spheres Go Here");
 
