ac884c3f68bfa986a0574d824f07c4989eb0c20d
Hans <htvanlierop@loyola.edu>
2024-02-14 14:05:51 -0500

    Added user input

--- Main.java
+++ Main.java
@@ -16,9 +16,9 @@ class Main {
        System.out.println("Music of the Spheres\n");
 
        // Initialize three spheres
-       Sphere sun = new Sphere("sun", 43.26, 0.0, 0.0, 0.0);
-       Sphere earth = new Sphere("earth", 0.395, 26.0, 42.0, 79.0);
-       Sphere planetX = new Sphere("planetX", 1.4, 25.3, 43.1, 77.8);
+        Sphere sun = new Sphere("sun", 43.26, 0.0, 0.0, 0.0);
+        Sphere earth = new Sphere("earth", 0.395, 26.0, 42.0, 79.0);
+        Sphere planetX = new Sphere("planetX", 1.4, 25.3, 43.1, 77.8);
 
        // Print surface area and volume of the three spheres
         System.out.println(sun.name + ":\nSurface Area - " + sun.surfaceArea() + "\nVolume - " + sun.volume() + "\n");
@@ -27,13 +27,49 @@ class Main {
 
         // Collision check
         if (sun.collision(earth.radius, earth.x, earth.y, earth.z)) {
-            System.out.println("The sun and earth's spheres have collided.");
+            System.out.println("The sun and earth's spheres have collided.\n");
         }
         if (sun.collision(planetX.radius, planetX.x, planetX.y, planetX.z)) {
-            System.out.println("The sun and planetX's spheres have collided.");
+            System.out.println("The sun and planetX's spheres have collided.\n");
         }
         if (planetX.collision(earth.radius, earth.x, earth.y, earth.z)) {
-            System.out.println("PlanetX and earth's spheres have collided.");
+            System.out.println("PlanetX and earth's spheres have collided.\n");
         }
+
+        // Get user input for a 3D location
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter an x coordinate: ");
+        while (!input.hasNextDouble()) {
+            System.out.println("Enter a double value: ");
+            input.next();
+        }
+        double loc_x = input.nextDouble();
+
+        System.out.println("Enter a y coordinate: ");
+        while (!input.hasNextDouble()) {
+            System.out.println("Enter a double value: ");
+            input.next();
+        }
+        double loc_y = input.nextDouble();
+
+        System.out.println("Enter a z coordinate: ");
+        while (!input.hasNextDouble()) {
+            System.out.println("Enter a double value: ");
+            input.next();
+        }
+        double loc_z = input.nextDouble();
+
+        // Check which sphere the user's location is closest to
+        double distanceToSun = sun.distance(loc_x, loc_y, loc_z);
+        double distanceToEarth = earth.distance(loc_x, loc_y, loc_z);
+        double distanceToPlanetX = planetX.distance(loc_x, loc_y, loc_z);
+        if (distanceToSun < distanceToEarth && distanceToSun < distanceToPlanetX) {
+            System.out.println("The location is closest to the Sun.");
+        } else if (distanceToEarth < distanceToSun && distanceToEarth < distanceToPlanetX) {
+            System.out.println("The location is closest to the Earth.");
+        } else {
+            System.out.println("The location is closest to planetX.");
+        }
+
     }
 }
