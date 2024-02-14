34cccb23804d67607a457719e2c1c3f9eee1af7e
Hans <htvanlierop@loyola.edu>
2024-02-14 13:19:12 -0500

    Added constructors for sphere class

--- Sphere.java
+++ Sphere.java
@@ -12,6 +12,30 @@
 
 
 class Sphere {
+    private double radius, x, y, z;
+    // General Constructor
+    public Sphere(double radius, double x, double y, double z) {
+        this.radius = radius;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    // Default Constructor
+    public Sphere(){
+        this.radius = 1;
+        this.x = 0;
+        this.y = 0;
+        this.z = 0;
+    }
+
+    // Surface area method
+    public double surfaceArea() {
+        return 4 * Math.PI * Math.pow(radius, 2);
+    }
+    // Volume method
+    // Distance method
+    // Collision method
     public static void main(String[] args) {
        System.out.println("Spheres Go Here");
 
